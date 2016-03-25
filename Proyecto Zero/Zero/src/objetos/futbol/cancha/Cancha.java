604dd94416a7714c236e20414c2e693e7213e80d
Camilo <juan-camilo-lm@hotmail.com>
2016-03-25 12:03:54 -0500

    Update Cancha.java

--- Proyecto Zero/Zero/src/objetos/futbol/cancha/Cancha.java
+++ Proyecto Zero/Zero/src/objetos/futbol/cancha/Cancha.java
@@ -2,7 +2,7 @@ package objetos.futbol.cancha;
 import java.lang.Math;//para poder usar seno y coseno
 
 class Cancha {
-	//se est· trabajando en milÌmetros
+	//se est√° trabajando en mil√≠metros
 	static final double largo=1830;
 	static final double ancho=1220;
 	
@@ -11,7 +11,7 @@ class Cancha {
 	private int DireccionRobot;//no se sabe si entero, double o String
 	
 	private final double constante_angulo=0; //no es 0, hay que calvularla con la formula
-	/*((360*Diametros_ruedas*cambio_tacometro)/Largo_llantas)*/
+	/*((360*Diametros_ruedas)/Largo_llantas)*/
 	
 	
 	public Cancha(/*como parametro los motores o algun referente*/){
@@ -35,20 +35,31 @@ class Cancha {
 		return true;
 	}
 	private double calcular_x(){
-		//operaciones geomÈtricas con coseno
+		//operaciones geom√©tricas con coseno
+		/*
+			m=DiametroLlantas*Pi*cambio_Tacometro
+			xf= ((cos(angulo))*(m)) +x0(posicoin anterior en x)
+		*/
 		double x=0;
 		return x;
 	}
 	private double calcuar_y(){
 		//operaciones geometricas con seno
+		/*
+			m=DiametroLlantas*Pi*cambio_Tacometro
+			yf= ((sen(angulo))*(m)) +x0(posicoin anterior en y)
+		*/
 		double y=0;
 		return y;
 	}
 	private int calcular_dir(){
-		//operaciones con la constante 
+		//furmulas
+		//(constante_angulo*cambio_Tacometro)+ angulo0(angulo anterior)(es + si se gira a la izquierdo)
+		//(constante_angulo*cambio_Tacometro)- angulo0(angulo anterior)(es - si se gira a la derecha)
 		int dir=0;
 		return dir;
 	}
+	//set (no se si quitarlos)
 	public void setPosicionRobot_x(double Posicion_Robotx){
 		this.PosicionRobot_x=Posicion_Robotx;
 	}
@@ -58,5 +69,16 @@ class Cancha {
 	public void setDireccionRobot(int DireccionRobot){
 		this.DireccionRobot=DireccionRobot;
 	}
+	//get (no se si quitarlos)
+	public void getPosicionRobot_x(){
+		return PosicionRobot_x;
+	}
+	public void getPosicionRobot_y(){
+		return PosicionRobot_y;
+	}
+	public void getDireccionRobot(){
+		return DireccionRobot;
+	}
+	
 	
 }
