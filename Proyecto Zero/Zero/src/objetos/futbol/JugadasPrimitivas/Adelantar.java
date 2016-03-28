518c43b712b0f31358d1c4e296a18394caf19c9f
Camilo <juan-camilo-lm@hotmail.com>
2016-03-28 17:11:27 -0500

    Update Adelantar.java

--- Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/Adelantar.java
+++ Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/Adelantar.java
@@ -1,8 +1,9 @@
-import lejos.nxt.*;//no sabemos especificamente que vamos a importar pero en este caso los motores
-
 package objetos.futbol.JugadasPrimitivas;
+import lejos.nxt.*;//no sabemos especificamente que vamos a importar pero en este caso los motores
+import objetos.futbol.cancha;
 
 class Adelantar extends jugadaPrimitiva {
+	
 	public Adelantar(int potencia, String nombre){
 		this.potencia= potencia;
 		this.nombre=nombre;
@@ -10,6 +11,7 @@ class Adelantar extends jugadaPrimitiva {
 		Motor.B.setSpeed(potencia);//supomenos que así se pone la velocidad con respespecto a la potencia
 	}
 	public void ejecutar(){
+		Delta_Taco=Motor.A.getTachoCount();
 		Motor.A.forward();//izquierdo=A
 		Motor.B.forward();//derecho=B
 		
@@ -17,8 +19,12 @@ class Adelantar extends jugadaPrimitiva {
 	}
 	public void parar(){
 		Motor.A.stop();
-		Motor.B.stop();
-		//detiene los motores
+		Motor.B.stop(); // D#16 No encontramos como calcular desde las jugadas primitivas la posición 
+		//X y Y en la cancha
+		
+	}
+	public static double Delta_PrimTaco(double a){
+		return Motor.A.getTachoCount()-a;
 		
 	}
 }
