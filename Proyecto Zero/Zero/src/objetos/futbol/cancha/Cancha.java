90d0cc550d930b51ebe4524aee44c353198c7619
Camilo <juan-camilo-lm@hotmail.com>
2016-03-28 17:14:40 -0500

    Update Cancha.java

--- Proyecto Zero/Zero/src/objetos/futbol/cancha/Cancha.java
+++ Proyecto Zero/Zero/src/objetos/futbol/cancha/Cancha.java
@@ -1,40 +1,50 @@
 package objetos.futbol.cancha;
 import java.lang.Math;//para poder usar seno y coseno
+import lejos.nxt.*;
+import objetos.futbol.Jugador;
+import lejos.nxt.remote.NXTCommand; // importamos la conexón bluethoot
+import lejos.pc.comm.*; 
 
 class Cancha {
 	//se está trabajando en milímetros
 	static final double largo=1830;
 	static final double ancho=1220;
 	
+	
 	private double PosicionRobot_x;
 	private double PosicionRobot_y;
 	private int DireccionRobot;//no se sabe si entero, double o String
-	
+	private NXTConnector conex = new NXTConnector(); 
 	private final double constante_angulo=0; //no es 0, hay que calvularla con la formula
 	/*((360*Diametros_ruedas)/Largo_llantas)*/
 	
 	
-	public Cancha(/*como parametro los motores o algun referente*/){
+	public Cancha(){
+		NXTCommandConnector.setNXTCommand(new NXTCommand(conex.getNXTComm())); //Comunica con el robot
+		Motor.A.resetTachoCount();
+		Motor.B.resetTachoCount();
+		Motor.C.resetTachoCount();
 		PosicionRobot_x=0;
 		PosicionRobot_y=0;
 		DireccionRobot=0;
-		/*se tienen que reinicar los tacometros de los motores*/
+		//Resetea los tacómetros, pone los valores en 0. (Constructor)
 	}
 	
 	public void IniciarRobot(){
 		PosicionRobot_x=0;
 		PosicionRobot_y=0;
 		DireccionRobot=0;
+		//Reinicia los valores de los atributos.
 	}
 	
-	public boolean calcularPosicionJugador(){//no se que pareametros pasar
+	public boolean calcularPosicionJugador(){//no recibe parámetros
 		//acturalizar los atributos con las funciones calcular
-		//ver si sa sale de la cancha o no
+		//Vemos si es delantero o portero
 		//si se sale return false, de lo contrario true
 		//utilizar las medidas
 		return true;
 	}
-	private double calcular_x(){
+	public double calcular_x(){
 		//operaciones geométricas con coseno
 		/*
 			m=DiametroLlantas*Pi*cambio_Tacometro
@@ -43,7 +53,7 @@ class Cancha {
 		double x=0;
 		return x;
 	}
-	private double calcuar_y(){
+	public double calcuar_y(){
 		//operaciones geometricas con seno
 		/*
 			m=DiametroLlantas*Pi*cambio_Tacometro
@@ -52,7 +62,7 @@ class Cancha {
 		double y=0;
 		return y;
 	}
-	private int calcular_dir(){
+	public int calcular_dir(){
 		//furmulas
 		//(constante_angulo*cambio_Tacometro)+ angulo0(angulo anterior)(es + si se gira a la izquierdo)
 		//(constante_angulo*cambio_Tacometro)- angulo0(angulo anterior)(es - si se gira a la derecha)
