4e55d00c6d96d6de1df661de7d1237a1f1fabeb0
Camilo <juan-camilo-lm@hotmail.com>
2016-03-28 15:07:24 -0500

    Update Adelantar.java

--- Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/Adelantar.java
+++ Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/Adelantar.java
@@ -1,16 +1,24 @@
+import lejos.nxt.*;//no sabemos especificamente que vamos a importar pero en este caso los motores
+
 package objetos.futbol.JugadasPrimitivas;
 
 class Adelantar extends jugadaPrimitiva {
 	public Adelantar(int potencia, String nombre){
 		this.potencia= potencia;
 		this.nombre=nombre;
+		Motor.A.setSpeed(potencia);
+		Motor.B.setSpeed(potencia);//supomenos que así se pone la velocidad con respespecto a la potencia
 	}
 	public void ejecutar(){
-		//pendiente!!! con los motores
+		Motor.A.forward();//izquierdo=A
+		Motor.B.forward();//derecho=B
+		
 		//los dos motores hacia adelante
 	}
 	public void parar(){
-		//Stop() ambos motores (PENDIENTE!!)
+		Motor.A.stop();
+		Motor.B.stop();
+		//detiene los motores
 		
 	}
 }
