8ec54796b3994d1a909f0fd2393e4560fddb4e06
Camilo <juan-camilo-lm@hotmail.com>
2016-03-28 16:57:57 -0500

    Update jugadaPrimitiva.java

--- Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/jugadaPrimitiva.java
+++ Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/jugadaPrimitiva.java
@@ -2,6 +2,8 @@ package objetos.futbol.JugadasPrimitivas;
 abstract class jugadaPrimitiva {
 	String nombre; //revisar si se hacen public o default
 	int potencia;
+	static double Delta_Taco; //Delta del tacómetro, lo utilizamos en cada clase para 
+	                      //encontrar las componentes de X y Y en la cancha
 	public abstract void ejecutar();
 	public abstract void parar();
 	public String getInformacion(){
