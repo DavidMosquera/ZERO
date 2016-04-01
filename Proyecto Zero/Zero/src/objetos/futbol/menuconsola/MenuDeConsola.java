caa75e413bb2e621d1111d359de329c22896a3fd
Camilo <juan-camilo-lm@hotmail.com>
2016-04-01 08:56:30 -0500

    Update MenuDeConsola.java

--- Proyecto Zero/Zero/src/objetos/futbol/menuconsola/MenuDeConsola.java
+++ Proyecto Zero/Zero/src/objetos/futbol/menuconsola/MenuDeConsola.java
@@ -8,51 +8,52 @@ public class MenuDeConsola {
 	private ArrayList<OpcionDeMenu> Opciones = new ArrayList<OpcionDeMenu>();
 	public void lanzarMenu(){
 		NXTConnector conn = new NXTConnector();
-		byte numero, memoria=-1;
 		Scanner scanner = new Scanner(System.in); // Para leer la opción seleccionada
 		
+		//atributos:
+		byte numero, memoria=-1;
 		Lista_opc(Opciones);//Agrega las opciones a la lista
+		boolean rol;
 		
 		System.out.println("Conectado: "+ conn.connectTo());
 		NXTCommandConnector.setNXTCommand(new NXTCommand(conn.getNXTComm())); 
-		while(true){
-			String memoria ="0";
-			System.out.println("Opciones: "+"\n"+"(0) Conectar "+"\n"+"(1) Adelante "+ 
-					"\n"+"(2) Retroceder "+"\n"+"(3) Girar a la Izquierda "+"\n"+ 
-					"(4) Girar a la Derecha "+"\n"+"(5) Patear "+"\n"+"(6) Frenar"+"\n"+
-					"(7) Salir");
-			String numero = scanner.next();//lo cambie para no confundirnos con opc o opcion
-			/*se usa un String para que no nos salte error i ingresamos un String por eroor, solo un 
-			aviso por consola*/
-			
-			detener_accion(memoria);
-			opcion(numero);
-			memoria=numero;
-			
-			
-			
+		
+		//primero se le pregunta al usuario si es jugador o admin 
+		rol= admin_o_jugador();//false si es admin, true si es jugador
+		if(rol==true){
+			while(true){
+				String memoria ="0";
+				imprimir_opciones_jugador();//imprime el menú con las opciones de el jugador
+				String numero = scanner.next();//lo cambie para no confundirnos con opc o opcion
+				/*se usa un String para que no nos salte error i ingresamos un String por eroor, solo un 
+				aviso por consola*/
+				
+				detener_accion(memoria);
+				opcion(numero);
+				memoria=numero;
+			}
 		}
 		private void opcion(String opc_numero){
 			if (opc_numero.compareTo("0")==0){ // Condicional para identificar la opción ingresada
-				Opciones[0].ejecutar(); // Por el usuario
+				Opciones.get(0).ejecutar(); // Por el usuario
 			}
 			else if (opc_numero.compareTo("1")==0){ //Ejecuta opcion # 1 (adelante)
-				Opciones[1].ejecutar(); 	
+				Opciones.get(1).ejecutar(); 	
 			}
 			else if (opc_numero.compareTo("2")==0){ //Ejecuta opcion # 2 (retroceder)
-				Opciones[2].ejecutar(); 
+				Opciones.get(2).ejecutar(); 
 			}
 			else if (opc_numero.compareTo("3")==0){ //Ejecuta opcion # 3 (Girar Izquierda)
-				Opciones[3].ejecutar(); 
+				Opciones.get(3).ejecutar(); 
 			}
 			else if (opc_numero.compareTo("4")==0){ //Ejecuta opcion # 4 (Girar Derecha)
-				Opciones[4].ejecutar(); 
+				Opciones.get(4).ejecutar(); 
 			}
 			else if (opc_numero.compareTo("5")==0){ //Ejecuta opcion # 5 (Patear)
-				Opciones[5].ejecutar(); 
+				Opciones.get(5).ejecutar(); 
 			}
 			else if (opc_numero.compareTo("6")==0){ //Ejecuta opcion # 6 (Frenar)
-				Opciones[6].ejecutar(); 	
+				Opciones.get(6).ejecutar(); 	
 			}
 			else if (opc_numero.compareTo("7")==0){ //Ejecuta opcion # 7 (Salir)
 				break;
@@ -64,19 +65,19 @@ public class MenuDeConsola {
 		}
 		private void detener_accion (String m){
 			if (m.compareTo("1")==0)){          //Ejecuta el método parar de Adelantar
-				Opciones[1].parar();
+				Opciones.get(1).parar();
 			}
 			else if (m.compareTo("2")==0)){    //Ejecuta el método parar de Retroceder
-				Opciones[2].parar();
+				Opciones.get(2).parar();
 			}
 			else if (m.compareTo("3")==0)){   //Ejecutar el método parar de Girar Izquierda
-				Opciones[3].parar();
+				Opciones.get(3).parar();
 			}
 			else if (m.compareTo("4")==0)){   //Ejecutar el método parar de Girar Derecha
-				Opciones[4].parar();
+				Opciones.get(4).parar();
 			}
 			else if (m.compareTo("5")==0)){   //Ejetcua el método parar de Patear
-				Opciones[5].parar();
+				Opciones.get(5).parar();
 			}
 			else{ // No se realiza nada
 				
@@ -94,6 +95,32 @@ public class MenuDeConsola {
 		}
 		public void anadirOpcion(OpcionDeMenu r){
 		Opciones.add(r);
+	        }
+	        //imprime las opciones del jugador 
+	        private void imprimir_opciones_jugador(){
+	        	System.out.println("Opciones: "+"\n"+"(0) Conectar "+"\n"+"(1) Adelante "+ 
+				"\n"+"(2) Retroceder "+"\n"+"(3) Girar a la Izquierda "+"\n"+ 
+				"(4) Girar a la Derecha "+"\n"+"(5) Patear "+"\n"+"(6) Frenar"+"\n"+
+				"(7) Salir");
+	        }
+	        //este metodo imprime el menú de bienvenida y pregunta el rol
+	        private boolean admin_o_jugador(){
+	        	while(true){
+	        		System.out.println("Bienvenido, porfavor ejila su rol:"+"/n"+"(1) Jugador"+"(2) Administrador");
+	        		numero= scanner.next();
+	        		if(numero.compareTo("1")=0){
+	        			return true;
+	        		}
+	        		else if(numero.compareTo("2")=0){
+	        			return false;
+	        		}
+	        		else{
+	        			System.out.printl("ERROR NUMERO INGRESADO NO VÁLIDO");
+	        		}
+	        	}
+	        	
+	        	
+	        	
 	        }
 	}
 }
