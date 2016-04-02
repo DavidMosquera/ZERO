088caad0654f3c3a07db79e35913a63bf38e8071
Jusariasve <jusariasve@unal.edu.co>
2016-04-02 12:10:06 -0500

    Update Opcion_adelante_w.java

--- Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Opcion_adelante_w.java
+++ Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Opcion_adelante_w.java
@@ -4,7 +4,7 @@ import objetos.futbol.JugadasPrimitivas.Adelantar;
 
 //adelante
 public class Opcion_adelante_w extends OpcionDeMenu {
-	private Adelantar Adelanta = new Adelantar(1080, "Adelantar");
+	private Adelantar Adelanto = new Adelantar(1080, "Adelantar");
 
 	public void ejecutar() {
 		J.ejecutar();
