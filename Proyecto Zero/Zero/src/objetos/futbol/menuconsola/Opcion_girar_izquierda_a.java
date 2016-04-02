2028566a04fbdeb8c02d10a28a759da35200cdb9
Jusariasve <jusariasve@unal.edu.co>
2016-04-02 12:27:27 -0500

    Update Opcion_girar_izquierda_a.java

--- Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Opcion_girar_izquierda_a.java
+++ Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Opcion_girar_izquierda_a.java
@@ -7,10 +7,10 @@ public class Opcion_girar_izquierda_a extends OpcionDeMenu {
 	private GirarIzquierda Giros_Izquierda = new GirarIzquierda(1080, "Girar a la Izquierda");
 
 	public void ejecutar() {
-		J.ejecutar();
+		Giros_Izquierda.ejecutar();
 	}
 
 	public void parar() {
-		J.parar();
+		Giros_Izquierda.parar();
 	}
 }
