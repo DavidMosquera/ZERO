99213baafa948361930872a8efed3c0d5513b48d
Neokilling <jdmosquerat@unal.edu.co>
2016-04-02 13:17:52 -0500

    Update GirarIzquierda.java

--- Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/GirarIzquierda.java
+++ Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/GirarIzquierda.java
@@ -1,5 +1,6 @@
 package objetos.futbol.JugadasPrimitivas;
 import lejos.nxt.*;
+import objetos.futbol.cancha.Cancha;
 
 public class GirarIzquierda extends jugadaPrimitiva {
 	public GirarIzquierda (int potencia, String nombre){
@@ -9,11 +10,14 @@ public class GirarIzquierda extends jugadaPrimitiva {
 		Motor.B.setSpeed(potencia);
 	}
 	public void ejecutar(){
-		Motor.A.backward();//izquierdo
-		Motor.B.forward();//derecho
+		Cancha.Memoria_taco=Motor.A.getTachoCount();
+		Cancha.Memoria_angulo=Cancha.Angulo_actual;
+		Motor.A.forward();//Derecho
+		Motor.B.backward();//Izquierdo
 		//motorA hacia atras y motorB hacia adelante
 	}
 	public void parar(){
+		Cancha.Angulo_actual=Cancha.calcular_dir_izquierda();
 		Motor.A.stop();
 		Motor.B.stop();
 		//parar ambos motores	
