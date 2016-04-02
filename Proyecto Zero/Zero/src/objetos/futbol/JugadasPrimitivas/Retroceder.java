a387ee7c786035f28fe9be77162829988aeb85a3
Neokilling <jdmosquerat@unal.edu.co>
2016-04-02 13:16:35 -0500

    Update Retroceder.java

--- Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/Retroceder.java
+++ Proyecto Zero/Zero/src/objetos/futbol/JugadasPrimitivas/Retroceder.java
@@ -1,5 +1,6 @@
 package objetos.futbol.JugadasPrimitivas;
 import lejos.nxt.*;//mirar descripcion en Adelantar
+import objetos.futbol.cancha.Cancha;
 
 public class Retroceder extends jugadaPrimitiva {
 	public Retroceder(int potencia, String nombre){
@@ -9,11 +10,15 @@ public class Retroceder extends jugadaPrimitiva {
 		Motor.B.setSpeed(potencia);
 	}
 	public void ejecutar(){
+		Cancha.Memoria_taco=Motor.A.getTachoCount(); //Guardamos El Tacometro
+		
 		Motor.A.backward();//izquierdo
 		Motor.B.backward();//derecho
-		//los dos motores hacia atrás
+		//los dos motores hacia atrÃ¡s
 	}
 	public void parar(){
+		Cancha.Delta_taco=-Motor.A.getTachoCount()+Cancha.Memoria_taco; //Encontramos el delta
+		Cancha.Fuera_de_posicion=Cancha.calcularPosicionJugador(); //Aplicamos la formula para las posiciones
 		Motor.A.stop();
 		Motor.B.stop();
 		//para los motores
