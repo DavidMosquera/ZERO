2e7b12c111194cdf342046b4bffde9368578d8b4
Jusariasve <jusariasve@unal.edu.co>
2016-04-02 12:06:45 -0500

    Update Opcion_girar_derecha_d.java

--- Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Opcion_girar_derecha_d.java
+++ Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Opcion_girar_derecha_d.java
@@ -4,7 +4,7 @@ import objetos.futbol.JugadasPrimitivas.GirarDerecha;
 
 //girar derecha
 public class Opcion_girar_derecha_d extends OpcionDeMenu {
-	private GirarDerecha J = new GirarDerecha(1080, "Girar a la Derecha");
+	private GirarDerecha Giros_Derecha = new GirarDerecha(1080, "Girar a la Derecha");
 
 	public void ejecutar() {
 		J.ejecutar();
