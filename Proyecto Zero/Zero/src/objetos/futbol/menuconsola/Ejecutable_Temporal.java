04c9ca8cba4e4b45d2f7197b6ae7362539d45295
Neokilling <jdmosquerat@unal.edu.co>
2016-04-03 17:38:06 -0500

    Update Ejecutable_Temporal.java

--- Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Ejecutable_Temporal.java
+++ Proyecto Zero/Zero/src/objetos/futbol/menuconsola/Ejecutable_Temporal.java
@@ -1,8 +1,12 @@
 package objetos.futbol.menuconsola;
-
+import java.io.FileNotFoundException;
+import objetos.futbol.cancha.Cancha;
 public class Ejecutable_Temporal {
-	public static void main (String[]args){
-		MenuDeConsola PRUEBA = new MenuDeConsola();
-		PRUEBA.lanzarMenu();
+	public static void main (String[]args) throws FileNotFoundException{
+		Cancha.llenarGrafico();
+		MenuDeConsola Alpha = new MenuDeConsola();
+		Cancha Cancha1 = new Cancha ();
+		Alpha.lanzarMenu();
+		
 	}
 }
