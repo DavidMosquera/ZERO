02eb2376c003693dab9b44610fd5a5a9ac16a2ec
Jusariasve <jusariasve@unal.edu.co>
2016-04-02 20:56:32 -0500

    Update JugadaComplejaDefensiva.java

--- Proyecto Zero/Zero/src/objetos/futbol/jugadores/JugadaComplejaDefensiva.java
+++ Proyecto Zero/Zero/src/objetos/futbol/jugadores/JugadaComplejaDefensiva.java
@@ -1,5 +1,27 @@
 package objetos.futbol.jugadores;
 
-public class JugadaComplejaDefensiva {
+import java.util.ArrayList;
+
+import lejos.nxt.Motor;
+import objetos.futbol.JugadasPrimitivas.jugadaPrimitiva;
+
+public class JugadaComplejaDefensiva extends JugadaCompleja{
+	int i;
+	public JugadaComplejaDefensiva(String NomJug, String FechCrea, UsuarioAdministrador Aut, ArrayList<jugadaPrimitiva> List, String Expli){
+		super(NomJug,FechCrea,Aut, List,Expli);
+	}
+	public void ejecutar(){
+		for (i=0;i<this.listaJugadasP.size();i++){
+			this.listaJugadasP.get(i).ejecutar();
+		}
+	}
+    public void parar(){
+    	Motor.A.stop();
+		Motor.B.stop();
+		Motor.C.stop(); //Aca tendriamos que hacer que el brazo vuelva a la posicion inicial, ya que puede parase mientras lo levanta
+	}
+    public String IdentificarJugada(){
+	    return "Jugada Defensiva";
+    }
 
 }
