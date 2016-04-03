32fe2b92039d7dde47d7a6e99da7009911943e7a
Jusariasve <jusariasve@unal.edu.co>
2016-04-02 20:52:27 -0500

    Update JugadaCompleja.java

--- Proyecto Zero/Zero/src/objetos/futbol/jugadores/JugadaCompleja.java
+++ Proyecto Zero/Zero/src/objetos/futbol/jugadores/JugadaCompleja.java
@@ -1,6 +1,40 @@
 package objetos.futbol.jugadores;
-//Clase vacía, únicamente para que no exitan problemas de invocación en las otras clases 
-//por el momento (!PENDIENTE)
-public class JugadaCompleja {
+import java.util.*;
+import objetos.futbol.JugadasPrimitivas.*;
 
+public abstract class JugadaCompleja {
+	String nombreJugada;//revisar bien encapsulamiento-No se debe cambiar luego de establecer
+	String fechaCreacion; //dia-mes-aÃ±o -----No se debe cambiar luego de establecer
+	String Explicacion;
+    ArrayList <jugadaPrimitiva> listaJugadasP;
+	UsuarioAdministrador Autor; //Tenemos que revisar donde lo creamos -----No se debe cambiar luego de establecer
+
+	public JugadaCompleja(String NomJug, String FechCrea, UsuarioAdministrador Aut, ArrayList<jugadaPrimitiva> List, String Expli){
+		this.nombreJugada = NomJug;
+		this.fechaCreacion = FechCrea;
+		this.Autor = Aut;
+		this.listaJugadasP = List;
+		this.Explicacion = Expli;
+	}
+	
+	public String toString(){
+		return "Jugada " +this.nombreJugada ;
+	}
+	
+	public boolean equals (JugadaCompleja JC){
+		if (JC.listaJugadasP.size()==this.listaJugadasP.size()){
+			if (this.listaJugadasP.equals(JC.listaJugadasP)){
+				return true;
+			}
+		}
+		else{
+			return false;
+		}
+		
+	}
+	public abstract String IdentificarJugada();
+	public abstract void ejecutar(); // en las 3 subclases dice en el texto que antes de ejecutar se tiene que calcular la posicion del robot y que no 
+	//infirnja las normas y luego ejecutar la jugada
+	public abstract void parar();
+	
 }
