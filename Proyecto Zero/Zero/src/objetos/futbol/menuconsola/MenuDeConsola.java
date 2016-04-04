b16649ae20e8181ef8ea63b9de8102648976a291
Camilo <juan-camilo-lm@hotmail.com>
2016-04-03 23:35:55 -0500

    Update MenuDeConsola.java

--- Proyecto Zero/Zero/src/objetos/futbol/menuconsola/MenuDeConsola.java
+++ Proyecto Zero/Zero/src/objetos/futbol/menuconsola/MenuDeConsola.java
@@ -1,18 +1,14 @@
 package objetos.futbol.menuconsola;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 import lejos.nxt.*;
 import lejos.nxt.remote.NXTCommand;
 import lejos.pc.comm.*;
+import objetos.futbol.cancha.Cancha;
 import objetos.futbol.jugadores.Arquero;
 import objetos.futbol.jugadores.Delantero;
 import objetos.futbol.jugadores.JugadaCompleja;
-import objetos.futbol.cancha.Cancha;
 
 public class MenuDeConsola {
 
@@ -28,19 +24,12 @@ public class MenuDeConsola {
 														// seleccionada
 	private Delantero delantero;
 	private Arquero arquero;
-	
-	File Jugadas = new File ("Jugadas.txt"); //Aca es donde le asignamos a el atributo Jugadas que se vincule con el documento
-	FileOutputStream Archivo = new FileOutputStream(Jugadas); //Se crea el objeto que que nos relaciona el documento con el objeto que escribira sobre el
-	PrintStream Escritor = new PrintStream(Archivo); //Este sera el objeto que empezara a escribir la informacion en el documento.
-	
 
 	public void lanzarMenu() {
-		
 		NXTConnector conn = new NXTConnector();
 		System.out.println("Conectado: " + conn.connectTo());
 		NXTCommandConnector.setNXTCommand(new NXTCommand(conn.getNXTComm()));
-		
-		lista_opc(Opciones);
+		Lista_opc();//toca descomentar las opciones para iniciar
 		setLista_arqueros();
 		setLista_delanteros();
 		// primero se le pregunta al usuario si es jugador o admin
@@ -49,84 +38,167 @@ public class MenuDeConsola {
 									// salir
 			if (rol == 1) {
 				lanzar_usuario();
-			} 
-			else if (rol == 2) {
+			} else if (rol == 2) {
 				while (true) {
 					// aquí van las opciones del administrador
 				}
-			}
-			else {
+			} else {
 				break;
 			}
 		}
 
 	}
+
+	// metodo interno de el lanzar_menú para usuario
+	//URGENTE
 	
-	
-	
-	
-	//metodo interno de el lanzar_menú para usuario 
-	private void lanzar_usuario(){
+	//DIVIDIR ESTE MOTODO EN OTROS
+	private void lanzar_usuario() {
 		boolean confirmacion = true;
 		while (confirmacion) {
-			
+			// se cambia la ubicación para que solo el usuario se
+			// conecte al
+			// robot
+			NXTConnector conn = new NXTConnector();
+			System.out.println("Conectado: " + conn.connectTo());
+			NXTCommandConnector.setNXTCommand(new NXTCommand(conn.getNXTComm()));
 
 			// elegir los jugadores
-			while(confirmacion){
-				confirmacion = elegir_arquero();//true si elige jugador, false si le da atras
-				if(confirmacion){
-					confirmacion=elegir_delantero();
-					if(confirmacion){
+			while (confirmacion) {
+				confirmacion = elegir_arquero();// true si elige jugador, false
+												// si le da atras
+				if (confirmacion) {
+					confirmacion = elegir_delantero();
+					if (confirmacion) {
 						break;
-					}
-					else{
-						confirmacion=true;
-					}
+					} else {
+						confirmacion = true;
 					}
 				}
-			
+			}
+			boolean  arquero_delantero=false; //FALSE = ARQUERO    TRUE = DELANTERO
+			String memoria = "0";
 			while (confirmacion) {
-				String memoria = "0";
-				imprimir_opciones_jugador();// imprime el menú con las
-											// opciones
-											// de el jugador
-				String action= scanner.next();
+				//confirma si el jugador se encuentra en una posicion adecuada, he imprime su respectivo menú puede
+				//haber problemas después con esta parte REVISAR
+				//si no esta en posicoin adecuada le pide que cambie de jugaros
+			
+				//desde aca partir
+				if(Cancha.Fuera_de_posicion==false){
+					
+				//partir 2
+				//menu diferente para arquero y delantero
+				//si esta dentro de la cancha pero con jugador no permitido que cambie
+					if(Cancha.calcularPosicionJugador()==false&&arquero_delantero==false){
+					
+						imprimir_opciones_jugador(false);//cambiar a opciones de menu arquero
+						String action2 = scanner.next();
+						detener_accion(memoria);
+						opcion(action2,false);//
+						memoria = action2;
+					}
+					else if(Cancha.calcularPosicionJugador()==true&&arquero_delantero==true){
+						imprimir_opciones_jugador(true);//cambiar a opciones de menu arquero
+						String action2 = scanner.next();
+						detener_accion(memoria);
+						opcion(action2,true);
+						memoria = action2;
+						
+					}
+					else{
+						
+					//partir3
+						if(Cancha.calcularPosicionJugador()==false){
+							System.out.println("Posicion incorrecta porfavor cambie de jugador a portero");
+							System.out.println("(1) cambiar a portero");
+							String basura = scanner.next();
+							while(true){
+								if(basura.compareTo("1")==0){
+									arquero_delantero=false;
+									break;
+								}
+								else{
+									System.out.println("ERROR NUMERO INGRESADO NO VÁLIDO");
+									basura= scanner.next();
+								}
+							}
+							
+						}
+						else{
+							System.out.println("Posicion incorrecta porfavor cambie de jugador a delantero");
+							System.out.println("(1) cambiar a delantero");
+							String basura = scanner.next();
+							while(true){
+								if(basura.compareTo("1")==0){
+									arquero_delantero=true;
+									break;
+								}
+								else{
+									System.out.println("ERROR NUMERO INGRESADO NO VÁLIDO");
+									basura= scanner.next();
+								}
+							}
+						}
+					}
+				}
+				//si el jugador se encuentra en posicion por fuera de la perimitida que se reinicien los contadores
+				//partir 4
+				else{
+					System.out.println("Jugador fuera de la cancha");
+					System.out.println("(1) reiniciar posicion");
+					String basura= scanner.next();
+					while(true){
+						if(basura.compareTo("1")==0){
+							Cancha.IniciarRobot(); //poner este metodo estatico check :)
+							break;
+						}
+						else{
+							System.out.println("ERROR NUMERO INGRESADO NO VÁLIDO");
+							basura= scanner.next();
+						}
+					}
+				}
 				
-
-				detener_accion(memoria);
-				opcion(action);
-				memoria = action;
 			}
 		}
 	}
-	
-	
-	
 
 	// no se si la opcion 7 es suficiente para salir
-	private void opcion(String opc_numero) {
-		 // Condicional para identificar la
+	private void opcion(String opc_numero,boolean arquero_o_delantero) {
+		
+		// Condicional para identificar la
 		// opción ingresado// Por el usuario
 		if (opc_numero.compareTo("w") == 0) { // Ejecuta opcion w
-														// (adelante)
-			Opciones.get(1).ejecutar();
+									// (adelante)
+			Opciones.get(0).ejecutar();
 		} else if (opc_numero.compareTo("s") == 0) { // Ejecuta opcion s
 														// (retroceder)
-			Opciones.get(2).ejecutar();
+			Opciones.get(1).ejecutar();
 		} else if (opc_numero.compareTo("a") == 0) { // Ejecuta opcion d (Girar
 														// Izquierda)
-			Opciones.get(3).ejecutar();
+			Opciones.get(2).ejecutar();
 		} else if (opc_numero.compareTo("d") == 0) { // Ejecuta opcion a (Girar
 														// Derecha)
-			Opciones.get(4).ejecutar();
+			Opciones.get(3).ejecutar();
 		} else if (opc_numero.compareTo("q") == 0) { // Ejecuta opcion q
 														// (Patear)
-			Opciones.get(5).ejecutar();
+			Opciones.get(4).ejecutar();
 		} else if (opc_numero.compareTo("e") == 0) { // Ejecuta opcion e
 														// (Frenar)
-			Opciones.get(6).ejecutar();
-		} else if (opc_numero.compareTo("t") == 0) { // Ejecuta opcion t (Salir)
-		} else {
+			Opciones.get(5).ejecutar();
+		} else if (opc_numero.compareTo("t") == 0) {
+			Opciones.get(6).ejecutar();// Ejecuta opcion t (Salir)
+//todavía falta agregar esa opcion de menú salir
+		}
+		else if(opc_numero.compareTo("1") == 0||opc_numero.compareTo("2") == 0||opc_numero.compareTo("3") == 0){
+				if(arquero_o_delantero){
+					//opciones jugadas complejas del DELANTERO
+				}
+				else{
+					//opciones jugadas complejas del ARQUERO
+				}
+		}
+		else {
 			System.out.println("ERROR NUMERO INGRESADO NO VÁLIDO");
 		}
 	}
@@ -150,14 +222,15 @@ public class MenuDeConsola {
 		}
 	}
 
-	private void Lista_opc(ArrayList<OpcionDeMenu> lista) {
+	private void Lista_opc() {
 		// Conectar
-		lista.add(new Opcion_adelante_w()); // Adelantar
-		lista.add(new Opcion_atras_s()); // Retroceder
-		lista.add(new Opcion_girar_izquierda_a()); // GiroIzquierda
-		lista.add(new Opcion_girar_derecha_d()); // GiroDerecha
-		lista.add(new Opcion_patear_q()); // Patear
-		lista.add(new Opcion_parar_e()); // Frenar
+		this.Opciones.add(new Opcion_adelante_w()); // Adelantar
+		this.Opciones.add(new Opcion_atras_s()); // Retroceder
+		this.Opciones.add(new Opcion_girar_izquierda_a()); // GiroIzquierda
+		this.Opciones.add(new Opcion_girar_derecha_d()); // GiroDerecha
+		this.Opciones.add(new Opcion_patear_q()); // Patear
+		this.Opciones.add(new Opcion_parar_e()); // Frenar
+//falta agreagar la opcion salir, la de las jugadas complejas 
 	}
 
 	public void anadirOpcion(OpcionDeMenu r) {
@@ -165,11 +238,27 @@ public class MenuDeConsola {
 	}
 
 	// imprime las opciones del jugador
-	private void imprimir_opciones_jugador() { //Matriz de Strings implementada en el menú para la localización. 
+	private void imprimir_opciones_jugador(boolean arquero_o_delantero) {
+		if(arquero_o_delantero){
+			String_cancha();
+			for(int i=0;i<delantero.listaJugadas.size();i++){
+				System.out.println("("+i+")"+delantero.listaJugadas.get(i).toString());
+				//duda en este metodo 
+			}
+		}
+		else{
+			String_cancha();
+			for(int i=0;i<arquero.listaJugadas.size();i++){
+				System.out.println("("+i+")"+arquero.listaJugadas.get(i).toString());
+				//duda en este metodo 
+			}		
+		}
+	}
+	private void String_cancha(){
 		System.out.println(               //Falta la implementación de las Jugadas complejas y las otras opciones.
 				"Opciones:"+		
 				"\n"+"¶═══════════════════════════════╦══════════════════════════════¶"+ 
-	                        "\n"+"║ "+Cancha.imprimir_fila_Grafico(0)+ "   ║ "+ "   (W) Adelante" +
+	            "\n"+"║ "+Cancha.imprimir_fila_Grafico(0)+ "   ║ "+ "   (W) Adelante" +
 				"\n"+"║ "+Cancha.imprimir_fila_Grafico(1)+ "   ║ "+ "   (S) Retroceder"+
 				"\n"+"║ "+Cancha.imprimir_fila_Grafico(2)+ "   ║ "+ "   (A) Girar Izquierda"+
 				"\n"+"║ "+Cancha.imprimir_fila_Grafico(3)+ "   ║ "+ "   (D) Girar Derecha"+
@@ -183,6 +272,7 @@ public class MenuDeConsola {
 				"\n"+"║ "+Cancha.imprimir_fila_Grafico(11)+"   ║ "+                          
 				"\n"+"║ "+Cancha.imprimir_fila_Grafico(12)+"   ║ "+                           
 				"\n"+"¶═══════════════════════════════╩══════════════════════════════¶ ");
+	
 	}
 
 	// este metodo imprime el menú de bienvenida y pregunta el rol
@@ -207,12 +297,12 @@ public class MenuDeConsola {
 	private boolean elegir_delantero() {
 		int contador = 0;
 		boolean comparar_accion = true;
-		System.out.println("\n"+"Elije tu delantero:");
+		System.out.println("\n" + "Elije tu delantero:");
 		while (comparar_accion) {
 			for (int i = 0; i < lista_delanteros.size(); i++) {
 				System.out.println("(" + (i + 1) + ")" + " " + lista_delanteros.get(i).getNombre());
 			}
-			System.out.println("(" + (lista_arqueros.size()+1) + ")" + " Atrás");
+			System.out.println("(" + (lista_arqueros.size() + 1) + ")" + " Atrás");
 			String accion = scanner.next();
 			for (int i = 0; i < lista_delanteros.size(); i++) {
 				contador = i + 1;
@@ -222,10 +312,10 @@ public class MenuDeConsola {
 					return true;
 				}
 			}
-			if(accion.compareTo(String.valueOf(lista_arqueros.size()+1))==0){
+			if (accion.compareTo(String.valueOf(lista_arqueros.size() + 1)) == 0) {
 				return false;
 			}
-			
+
 			if (comparar_accion == true) {
 				System.out.println("ERROR NUMERO INGRESADO NO VÁLIDO");
 			}
@@ -239,12 +329,12 @@ public class MenuDeConsola {
 	private boolean elegir_arquero() {
 		int contador = 0;
 		boolean comparar_accion = true;
-		System.out.println("\n"+"Elije tu arquero:");
+		System.out.println("\n" + "Elije tu arquero:");
 		while (comparar_accion) {
 			for (int i = 0; i < lista_arqueros.size(); i++) {
 				System.out.println("(" + (i + 1) + ")" + " " + lista_arqueros.get(i).getNombre());
 			}
-			System.out.println("(" + (lista_arqueros.size()+1) + ")" + " Atrás");
+			System.out.println("(" + (lista_arqueros.size() + 1) + ")" + " Atrás");
 			String accion = scanner.next();
 			for (int i = 0; i < lista_arqueros.size(); i++) {
 				contador = i + 1;
@@ -254,7 +344,7 @@ public class MenuDeConsola {
 					return true;
 				}
 			}
-			if(accion.compareTo(String.valueOf(lista_arqueros.size()+1))==0){
+			if (accion.compareTo(String.valueOf(lista_arqueros.size() + 1)) == 0) {
 				return false;
 			}
 			if (comparar_accion == true) {
@@ -265,16 +355,18 @@ public class MenuDeConsola {
 		return true;
 
 	}
-	private void setLista_arqueros(){
-		Arquero Monato= new Arquero("Monato","arquero",new ArrayList<JugadaCompleja>(),00,(byte)5);
+	//Desde acá se setean los arqueros y los delanteros default 
+	//falta poner las jugadas complejas, pero se supone que debe ser igual
+	//empecemos con los casos prueba 
+
+	private void setLista_arqueros() {
+		Arquero Monato = new Arquero("Monato", "arquero", new ArrayList<JugadaCompleja>(), 00, (byte) 5);
+		
 		this.lista_arqueros.add(Monato);
 	}
-	private void setLista_delanteros(){
-		Delantero Monatomate= new Delantero("Monatomate","Delantero",new ArrayList<JugadaCompleja>(),00,5);
+
+	private void setLista_delanteros() {
+		Delantero Monatomate = new Delantero("Monatomate", "Delantero", new ArrayList<JugadaCompleja>(), 00, 5);
 		this.lista_delanteros.add(Monatomate);
 	}
-	public MenuDeConsola() throws FileNotFoundException {
-		// Constructor necesario para poder asignar a Archivo el documento "Jugadas.txt"
-	} 
-	
 }
